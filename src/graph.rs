bc3e529cd7057288c66ee8c1cd0e598dc4502882
EbTech <aramebtech@gmail.com>
2017-06-04 18:58:41 -0700

    Added minimum spanning tree, strongly connected components, and 2-SAT solver

--- src/graph.rs
+++ src/graph.rs
@@ -11,14 +11,16 @@ impl DisjointSets {
     }
     
     pub fn find(&mut self, u: usize) -> usize {
-        let par_u = self.parent[u];
-        if par_u != u { self.parent[u] = self.find(par_u); }
+        let pu = self.parent[u];
+        if pu != u { self.parent[u] = self.find(pu); }
         self.parent[u]
     }
     
-    pub fn union(&mut self, u: usize, v: usize) {
+    // Returns true if u and v were previously in different sets.
+    pub fn union(&mut self, u: usize, v: usize) -> bool {
         let (pu, pv) = (self.find(u), self.find(v));
-        if pu != pv { self.parent[pu] = pv; }
+        self.parent[pu] = pv;
+        pu != pv
     }
 }
 
@@ -32,21 +34,37 @@ impl Graph {
     pub fn new(vmax: usize, emax: usize) -> Graph {
         Graph {
             first: vec![None; vmax],
-            next: Vec::with_capacity(2 * emax),
-            endp: Vec::with_capacity(2 * emax)
+            next: Vec::with_capacity(emax),
+            endp: Vec::with_capacity(emax)
         }
     }
     
-    pub fn add_edge(&mut self, a: usize, b: usize) {
-        for &u in &[a, b] {
-            self.next.push(self.first[u]);
-            self.first[u] = Some(self.endp.len());
-            self.endp.push(u);
-        }
+    pub fn num_v(&self) -> usize { self.first.len() }
+    pub fn num_e(&self) -> usize { self.next.len() }
+    
+    pub fn add_edge(&mut self, u: usize, v: usize) {
+        self.next.push(self.first[u]);
+        self.first[u] = Some(self.endp.len());
+        self.endp.push(v);
+    }
+    
+    pub fn add_undirected_edge(&mut self, u: usize, v: usize) {
+        self.add_edge(u, v);
+        self.add_edge(v, u);
+    }
+    
+    // Assumes odd-numbered vertices correspond to predecessors' negations.
+    // Logically equivalent forms: u || v, !u -> v, !v -> u
+    pub fn add_two_sat_clause(&mut self, u: usize, v: usize) {
+        self.add_edge(u^1, v);
+        self.add_edge(v^1, u);
     }
     
     pub fn adj_list<'a>(&'a self, u: usize) -> AdjListIterator<'a> {
-        AdjListIterator { graph: self, next_e: self.first[u] }
+        AdjListIterator {
+            graph: self,
+            next_e: self.first[u]
+        }
     }
 }
 
@@ -55,21 +73,33 @@ pub struct AdjListIterator<'a> {
     next_e: Option<usize>
 }
 
-impl<'a> ::std::iter::Iterator for AdjListIterator<'a> {
-    // produces an outgoing edge and vertex
+impl<'a> Iterator for AdjListIterator<'a> {
+    // Produces an outgoing edge and vertex.
     type Item = (usize, usize);
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next_e.map( |e| {
-            let v = self.graph.endp[e ^ 1];
+            let v = self.graph.endp[e];
             self.next_e = self.graph.next[e];
             (e, v)
         })
     }
 }
 
+// Assumes graph is undirected.
+pub fn min_spanning_tree(graph: &Graph, weights: &[i64]) -> Vec<usize> {
+    assert_eq!(graph.num_e(), 2 * weights.len());
+    let mut edges = (0..weights.len()).collect::<Vec<_>>();
+    edges.sort_by_key(|&i| weights[i]);
+    
+    let mut components = DisjointSets::new(graph.num_v());
+    edges.into_iter()
+        .filter(|&e| components.union(graph.endp[2*e], graph.endp[2*e+1]))
+        .collect()
+}
+
 #[derive(Clone)]
-pub struct FlowVertex {
+struct FlowVertex {
     lev: Option<usize>,
     cur: Option<usize> // TODO: consider making this an AdjListIterator
 }
@@ -82,7 +112,7 @@ pub struct FlowEdge {
 
 pub struct FlowGraph {
     pub graph: Graph,
-    pub vdata: Vec<FlowVertex>,
+    vdata: Vec<FlowVertex>,
     pub edata: Vec<FlowEdge>,
 }
 
@@ -90,7 +120,7 @@ impl FlowGraph {
     pub fn new(vmax: usize, emax: usize) -> FlowGraph {
         let data = FlowVertex { lev: None, cur: None };
         FlowGraph {
-            graph: Graph::new(vmax, emax),
+            graph: Graph::new(vmax, 2 * emax),
             vdata: vec![data; vmax],
             edata: Vec::with_capacity(2 * emax)
         }
@@ -101,7 +131,28 @@ impl FlowGraph {
         let rdata = FlowEdge { cost: -cost, ..data };
         self.edata.push(data);
         self.edata.push(rdata);
-        self.graph.add_edge(a, b);
+        self.graph.add_undirected_edge(a, b);
+    }
+    
+    fn dfs(&mut self, u: usize, t: usize, f: i64) -> i64 {
+        if u == t { return f; }
+        let mut df = 0;
+        
+        while let Some(e) = self.vdata[u].cur {
+            let v = self.graph.endp[e];
+            if let (Some(lu), Some(lv)) = (self.vdata[u].lev, self.vdata[v].lev) {
+                let rem_cap = self.edata[e].cap - self.edata[e].flow;
+                if rem_cap > 0 && lv == lu + 1 {
+                    let cf = self.dfs(v, t, ::std::cmp::min(rem_cap, f - df));
+                    self.edata[e].flow += cf;
+                    self.edata[e ^ 1].flow -= cf;
+                    df += cf;
+                    if df == f { break; }
+                }
+            }
+            self.vdata[u].cur = self.graph.next[e];
+        }
+        return df;
     }
     
     fn bfs(&mut self, s: usize, t: usize) -> bool {
@@ -118,28 +169,7 @@ impl FlowGraph {
                 }
             }
         }
-        return self.vdata[t].lev != None;
-    }
-    
-    fn dfs(&mut self, u: usize, t: usize, f: i64) -> i64 {
-        if u == t { return f; }
-        let mut df = 0;
-        
-        while let Some(e) = self.vdata[u].cur {
-            let v = self.graph.endp[e ^ 1];
-            if let (Some(lu), Some(lv)) = (self.vdata[u].lev, self.vdata[v].lev) {
-                let rem_cap = self.edata[e].cap - self.edata[e].flow;
-                if rem_cap > 0 && lv == lu + 1 {
-                    let cf = self.dfs(v, t, ::std::cmp::min(rem_cap, f - df));
-                    self.edata[e].flow += cf;
-                    self.edata[e ^ 1].flow -= cf;
-                    df += cf;
-                    if df == f { break; }
-                }
-            }
-            self.vdata[u].cur = self.graph.next[e];
-        }
-        return df;
+        self.vdata[t].lev != None
     }
     
     // Dinic's fast maximum flow: V^2E in general,
@@ -151,11 +181,159 @@ impl FlowGraph {
         }
         flow
     }
+    
+    pub fn min_cut(&self) -> Vec<usize> {
+        (0..self.graph.num_e()).filter( |&e| {
+            let u = self.graph.endp[e ^ 1];
+            let v = self.graph.endp[e];
+            self.vdata[u].lev.is_some() && self.vdata[v].lev.is_none()
+        }).collect()
+    }
+}
+
+// 2-vertex and 2-edge connected components
+// should handle multiple-edges and self-loops
+// USAGE: 1) new(); 2) add_edge(...); 3) compute_bcc();
+// 4) use is_cut_vertex(vertex_index) or is_cut_edge(2 * edge_index)
+
+#[derive(Clone)]
+pub struct CCVertex {
+    pub cc: usize,
+    low: usize,
+    vis: usize,
+}
+
+pub struct CCGraph<'a> {
+    pub graph: &'a Graph,
+    pub vdata: Vec<CCVertex>,
+    pub vcc: Vec<usize>,
+    pub n_cc: usize,
+    pub n_vcc: usize,
+    t: usize,
+    verts: ::std::collections::VecDeque<usize>,
+    //edges: ::std::collections::VecDeque<usize>
+}
+
+impl<'a> CCGraph<'a> {
+    pub fn new(graph: &'a Graph) -> CCGraph {
+        let data = CCVertex { cc: 0, low: 0, vis: 0 };
+        let mut cc_graph = CCGraph {
+            graph: graph,
+            vdata: vec![data; graph.num_v()],
+            vcc: vec![0; graph.num_e()],
+            n_cc: 0,
+            n_vcc: 0,
+            t: 0,
+            verts: ::std::collections::VecDeque::new(),
+            //edges: ::std::collections::VecDeque::new()
+        };
+        for i in 0..graph.num_v() {
+            if cc_graph.vdata[i].vis == 0 {
+                cc_graph.scc(i);
+            }
+        }
+        cc_graph
+    }
+    
+    // SCCs form a DAG whose components are numbered in reverse topological order.
+    fn scc(&mut self, u: usize) {
+        self.t += 1;
+        self.vdata[u].low = self.t;
+        self.vdata[u].vis = self.t;
+        self.verts.push_back(u);
+        for (_, v) in self.graph.adj_list(u) {
+            if self.vdata[v].vis == 0 { self.scc(v); }
+            if self.vdata[v].cc == 0 {
+                self.vdata[u].low = ::std::cmp::min(self.vdata[u].low, self.vdata[v].low);
+            }
+        }
+        if self.vdata[u].vis <= self.vdata[u].low {
+            self.n_cc += 1;
+            while let Some(v) = self.verts.pop_back() {
+                self.vdata[v].cc = self.n_cc;
+                if v == u { break; }
+            }
+        }
+    }
+    
+    pub fn two_sat_assign(&self) -> Option<Vec<bool>> {
+        let mut truth = Vec::with_capacity(self.graph.num_v()/2);
+        for i in 0..self.graph.num_v()/2 {
+            let scc_true = self.vdata[2*i].cc;
+            let scc_false = self.vdata[2*i+1].cc;
+            if scc_true == scc_false { return None; }
+            truth.push(scc_true < scc_false);
+        }
+        Some(truth)
+    }
+    
+    /*fn bcc(&mut self, u: usize, par: usize) {
+        self.t += 1;
+        self.vdata[u].low = self.t;
+        self.vdata[u].vis = self.t;
+        self.verts.push_back(u);
+        for (e, v) in self.graph.adj_list(u) {
+          if self.vdata[v] == None {
+              self.edges.push_back(e);
+              self.bcc(v, e);
+              self.vdata[u].low = ::std::cmp::min(self.vdata[u].low, self.vdata[v].low);
+              if self.vdata[u].vis <= self.vdata[v].low { // u is a cut vertex unless it's a one-child root
+                  do {
+                      let E = self.edges.top();
+                      self.edges.pop_back();
+                      vcc[E] = self.n_vcc;
+                      vcc[E^1] = self.n_vcc;
+                  } while e != E && e != (E^1);
+                  self.n_vcc += 1;
+              }
+          }
+          else if self.vdata[v].vis < self.vdata[u].vis && e != (par^1) {
+              self.vdata[u].low = ::std::cmp::min(self.vdata[u].low, self.vdata[v].vis);
+              self.edges.push_back(e);
+          }
+          else if v == u { // e is a self-loop
+              self.vcc[e] = self.n_vcc;
+              self.vcc[e ^ 1] = self.n_vcc;
+              self.n_vcc += 1;
+          }
+        } 
+        if self.vdata[u].vis <= self.vdata[u].low { // par is a cut edge unless par==-1
+            do { v = self.verts.top(); self.verts.pop_back(); self.vdata[v].cc = self.n_cc; }
+            while (v != u);
+            self.n_cc += 1; 
+        }
+    }
+    
+    pub fn is_cut_vertex(&self, u: usize) -> bool {
+        let vcc = self.vcc[self.graph.first[u]];
+        for (e, _) in self.graph.adj_list(u) {
+            if (self.vcc[e] != vcc) { return true; }
+        }
+        false
+    }
+    
+    pub fn is_cut_edge(&self, e: usize) -> bool {
+        let u = self.graph.endp[e ^ 1];
+        let v = self.graph.endp[e];
+        self.vdata[u].cc != self.vdata[v].cc
+    }*/
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    
+    #[test]
+    fn test_min_spanning_tree()
+    {
+        let mut graph = Graph::new(3, 3);
+        graph.add_undirected_edge(0, 1);
+        graph.add_undirected_edge(1, 2);
+        graph.add_undirected_edge(2, 0);
+        let weights = [7, 3, 5];
+        let mst = min_spanning_tree(&graph, &weights);
+        assert_eq!(mst, vec![1, 2]);
+    }
     
     #[test]
     fn test_basic_flow()
@@ -165,5 +343,21 @@ mod test {
         graph.add_edge(1, 2, 3, 1);
         let flow = graph.dinic(0, 2);
         assert_eq!(flow, 3);
+    }
+    
+    #[test]
+    fn test_two_sat()
+    {
+        let mut graph = Graph::new(6, 8);
+        let (x, y, z) = (0, 2, 4);
+        
+        graph.add_two_sat_clause(x, z);
+        graph.add_two_sat_clause(y^1, z^1);
+        graph.add_two_sat_clause(y, y);
+        assert_eq!(CCGraph::new(&graph).two_sat_assign(),
+                   Some(vec![true, true, false]));
+            
+        graph.add_two_sat_clause(z, z);
+        assert_eq!(CCGraph::new(&graph).two_sat_assign(), None);
     }
 }
