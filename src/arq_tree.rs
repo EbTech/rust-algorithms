97f73dec180d35cb00ffc744fcbd4b1a230465c4
Aram Ebtekar <aramebtech@gmail.com>
2019-06-21 19:21:10 -0700

    ArqTree eager and binary search examples using improved push/pull

--- src/arq_tree.rs
+++ src/arq_tree.rs
@@ -7,9 +7,15 @@
 ///
 /// - modify(l, r, f) replaces a_i (l <= i <= r) by f(a_i) for an endomorphism f
 /// - query(l, r) returns the aggregate a_l + a_{l+1} + ... + a_r
+///
+/// Future work: ArqTree would lend itself naturally to Rust's ownership system.
+/// Initially, we should only have access to the root nodes:
+///            if size is a power of two, there is a unique root at index 1.
+/// arq.push(i) locks i and acquires access to its children.
+/// arq.pull(i) is called when the lock on i is released.
 pub struct ArqTree<T: ArqSpec> {
-    d: Vec<Option<T::F>>,
-    t: Vec<T::M>,
+    app: Vec<Option<T::F>>,
+    val: Vec<T::M>,
 }
 
 impl<T: ArqSpec> ArqTree<T>
@@ -17,74 +23,82 @@ where
     T::F: Clone,
 {
     /// Initializes a static balanced tree on top of the given sequence.
-    pub fn new(init: Vec<T::M>) -> Self {
-        let size = init.len();
-        let mut t = (0..size).map(|_| T::identity()).collect::<Vec<_>>();
-        t.append(&mut { init });
-        for i in (0..size).rev() {
-            t[i] = T::op(&t[i << 1], &t[i << 1 | 1]);
+    pub fn new(init_val: Vec<T::M>) -> Self {
+        let size = init_val.len();
+        let mut val = (0..size).map(|_| T::identity()).collect::<Vec<_>>();
+        val.append(&mut { init_val });
+        let app = vec![None; size];
+
+        let mut arq = Self { app, val };
+        for p in (0..size).rev() {
+            arq.pull(p);
         }
-        Self {
-            d: vec![None; size],
-            t,
-        }
+        arq
     }
 
     fn apply(&mut self, p: usize, f: &T::F) {
-        self.t[p] = T::apply(f, &self.t[p]);
-        if p < self.d.len() {
-            let h = match self.d[p] {
+        self.val[p] = T::apply(f, &self.val[p]);
+        if let Some(lazy) = self.app.get_mut(p) {
+            let h = match *lazy {
                 Some(ref g) => T::compose(f, g),
                 None => f.clone(),
             };
-            self.d[p] = Some(h);
+            *lazy = Some(h);
         }
     }
 
     fn push(&mut self, p: usize) {
-        for s in (1..32).rev() {
-            let i = p >> s;
-            if let Some(ref f) = self.d[i].take() {
-                self.apply(i << 1, f);
-                self.apply(i << 1 | 1, f);
-            }
+        if let Some(ref f) = self.app[p].take() {
+            self.apply(p << 1, f);
+            self.apply(p << 1 | 1, f);
         }
     }
 
-    fn pull(&mut self, mut p: usize) {
+    fn pull(&mut self, p: usize) {
+        self.val[p] = T::op(&self.val[p << 1], &self.val[p << 1 | 1]);
+    }
+
+    fn push_to(&mut self, p: usize) {
+        for s in (1..32).rev() {
+            self.push(p >> s);
+        }
+    }
+
+    fn pull_from(&mut self, mut p: usize) {
         while p > 1 {
             p >>= 1;
-            if self.d[p].is_none() {
-                self.t[p] = T::op(&self.t[p << 1], &self.t[p << 1 | 1]);
-            }
+            self.pull(p);
         }
     }
 
     /// Applies the endomorphism f to all entries from l to r, inclusive.
+    /// If l == r, the updates are eager. Otherwise, they are lazy.
     ///
     /// # Panics
     ///
     /// Panics if l or r is out of range.
     pub fn modify(&mut self, mut l: usize, mut r: usize, f: &T::F) {
-        l += self.d.len();
-        r += self.d.len();
-        let (l0, r0) = (l, r);
-        self.push(l0);
-        self.push(r0);
+        l += self.app.len();
+        r += self.app.len();
+        self.push_to(l);
+        self.push_to(r);
+        let (mut l0, mut r0) = (1, 1);
         while l <= r {
             if l & 1 == 1 {
                 self.apply(l, f);
+                l0 = l0.max(l);
                 l += 1;
             }
             if r & 1 == 0 {
                 self.apply(r, f);
+                r0 = r0.max(r);
                 r -= 1;
             }
             l >>= 1;
             r >>= 1;
         }
-        self.pull(l0);
-        self.pull(r0);
+        self.pull_from(l0);
+        self.pull_from(r0);
     }
 
     /// Returns the aggregate range query on all entries from l to r, inclusive.
@@ -93,18 +107,18 @@ where
     ///
     /// Panics if l or r is out of range.
     pub fn query(&mut self, mut l: usize, mut r: usize) -> T::M {
-        l += self.d.len();
-        r += self.d.len();
-        self.push(l);
-        self.push(r);
+        l += self.app.len();
+        r += self.app.len();
+        self.push_to(l);
+        self.push_to(r);
         let (mut l_agg, mut r_agg) = (T::identity(), T::identity());
         while l <= r {
             if l & 1 == 1 {
-                l_agg = T::op(&l_agg, &self.t[l]);
+                l_agg = T::op(&l_agg, &self.val[l]);
                 l += 1;
             }
             if r & 1 == 0 {
-                r_agg = T::op(&self.t[r], &r_agg);
+                r_agg = T::op(&self.val[r], &r_agg);
                 r -= 1;
             }
             l >>= 1;
@@ -122,8 +136,11 @@ pub trait ArqSpec {
     type F;
     /// Type of monoid elements.
     type M;
+
+    /// For eager updates, compose() ho be unimplemented!(). For lazy updates:
     /// Require for all f,g,a: apply(compose(f, g), a) = apply(f, apply(g, a))
     fn compose(f: &Self::F, g: &Self::F) -> Self::F;
+    /// For eager updates, apply() can assume to act on a leaf. For lazy updates:
     /// Require for all f,a,b: apply(f, op(a, b)) = op(apply(f, a), apply(f, b))
     fn apply(f: &Self::F, a: &Self::M) -> Self::M;
     /// Require for all a,b,c: op(a, op(b, c)) = op(op(a, b), c)
@@ -132,78 +149,120 @@ pub trait ArqSpec {
     fn identity() -> Self::M;
 }
 
-/// Range Minimum Query, a classic application of associative range query.
+/// Range Minimum Query (RMQ), a classic application of ARQ.
+/// modify(l, r, &f) sets all entries a[l..=r] to f.
+/// query(l, r) finds the minimum value in a[l..=r].
+//
 // Exercises: try augmenting this struct to find the index of a minimum element
-// in a range query, as well as the number of elements that match the minimum.
-// Then instead of assigning to a range, try to support the operation of
-// incrementing each element in a range by a given offset!
+// in a range query, as well as the number of elements equal to the minimum.
+// Then instead of overwriting values with a constant assignment a[i] = f,
+// try supporting addition: a[i] += f.
 pub struct AssignMin;
-
 impl ArqSpec for AssignMin {
     type F = i64;
     type M = i64;
-    fn compose(f: &Self::F, _: &Self::F) -> Self::F {
-        *f
+    fn compose(&f: &Self::F, _: &Self::F) -> Self::F {
+        f
     }
-    fn apply(f: &Self::F, _: &Self::M) -> Self::M {
-        *f
+    fn apply(&f: &Self::F, _: &Self::M) -> Self::M {
+        f
     }
-    fn op(a: &Self::M, b: &Self::M) -> Self::M {
-        (*a).min(*b)
+    fn op(&a: &Self::M, &b: &Self::M) -> Self::M {
+        a.min(b)
     }
     fn identity() -> Self::M {
         Self::M::max_value()
     }
 }
 
-/// A slightly more advanced application, where we aim to support range sum
-/// queries and range constant assignments.
-/// Note that constant assignment f_c(a) = c is not a endomorphism on (i64, +)
-/// because f_c(a+b) = c != 2*c = f_c(a) + f_c(b). In intuitive terms, the
-/// problem is that the internal nodes of the tree should not simply be set to
-/// c, but rather, to c multiplied by the subtree size. So let's augment the
-/// monoid type with size information, using the 2D vector (a_i,1) instead of
-/// a_i. Now check that f_c((a, s)) = (c*s, s) is indeed an endomorphism on
-/// vector addition: f_c((a,s)+(b,t)) = f_c((a+b,s+t)) = (c*(s+t),s+t)
-///                 = (c*s,s)+(c*t,t) = f_c((a,s)) + f_c((b,t)).
+/// An example of binary search on an ArqTree.
+/// In this case, we use RMQ to locate the leftmost negative element.
+/// To ensure the existence of a valid root note (i == 1) from which to descend,
+/// the tree size must be a power of two.
+pub fn first_negative(arq: &mut ArqTree<AssignMin>) -> i32 {
+    assert!(arq.app.len().is_power_of_two());
+    let mut i = 1;
+    if arq.val[i] >= 0 {
+        return -1;
+    }
+    while i < arq.app.len() {
+        arq.push(i);
+        i <<= 1;
+        if arq.val[i] >= 0 {
+            i |= 1;
+        }
+    }
+    let pos = i - arq.app.len();
+    pos as i32
+}
+
+/// Range Sum Query, a slightly trickier classic application of ARQ.
+/// modify(l, r, &f) sets all entries a[l..=r] to f.
+/// query(l, r) sums all the entries a[l..=r].
 ///
 /// # Panics
 ///
 /// Associated functions will panic on overflow.
+//
+// Note that the apply() operation on raw entries is undefined: while leaf nodes
+// should simply be set to f, internal nodes must be set to f * size_of_subtree.
+// Thus, our monoid type M should store the pair (entry, size_of_subtree).
+//
+// In mathematical jargon, we say that constant assignment f(a) = f is not an
+// endomorphism on (i64, +) because f(a+b) = f != 2*f = f(a) + f(b).
+// On the other hand, f((a, s)) = (f*s, s) is indeed an endomorphism on pairs
+// with vector addition: f((a, s) + (b, t)) = f((a+b, s+t)) = (f*(s+t), s+t)
+//                       = (f*s, s) + (f*t, t) = f((a,s)) + f((b,t)).
 pub struct AssignSum;
-
 impl ArqSpec for AssignSum {
     type F = i64;
     type M = (i64, i64);
-    fn compose(f: &Self::F, _: &Self::F) -> Self::F {
-        *f
+    fn compose(&f: &Self::F, _: &Self::F) -> Self::F {
+        f
     }
-    fn apply(f: &Self::F, a: &Self::M) -> Self::M {
-        (f * a.1, a.1)
+    fn apply(&f: &Self::F, &(_, s): &Self::M) -> Self::M {
+        (f * s, s)
     }
-    fn op(a: &Self::M, b: &Self::M) -> Self::M {
-        (a.0 + b.0, a.1 + b.1)
+    fn op(&(a, s): &Self::M, &(b, t): &Self::M) -> Self::M {
+        (a + b, s + t)
     }
     fn identity() -> Self::M {
         (0, 0)
     }
 }
 
+/// Supply & Demand, based on https://codeforces.com/gym/102218/problem/F
+/// modify(i, i, &(p, o)) increases supply by p and demand by o at time i.
+/// query(l, r) computes total supply and demand at times l to r, as well as
+//              how much of the supply is subsequently met by the demand.
+//
+// Note that the apply() operation is only correct when applied to leaf nodes.
+// Therefore, modify() must only be used in "eager" mode, i.e., with l == r.
+// compose() should be unimplemented!() to prevent accidental "lazy" updates.
+pub struct SupplyDemand;
+impl ArqSpec for SupplyDemand {
+    type F = (i64, i64);
+    type M = (i64, i64, i64); // production, orders, sales
+    fn compose(_: &Self::F, _: &Self::F) -> Self::F {
+        unimplemented!()
+    }
+    fn apply(&(p_add, o_add): &Self::F, &(p, o, _): &Self::M) -> Self::M {
+        let p = p + p_add;
+        let o = o + o_add;
+        (p, o, p.min(o))
+    }
+    fn op((p1, o1, s1): &Self::M, (p2, o2, s2): &Self::M) -> Self::M {
+        let extra = (p1 - s1).min(o2 - s2);
+        (p1 + p2, o1 + o2, s1 + s2 + extra)
+    }
+    fn identity() -> Self::M {
+        (0, 0, 0)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_range_sum() {
-        let mut arq = ArqTree::<AssignSum>::new(vec![(0, 1); 10]);
-
-        assert_eq!(arq.query(0, 9), (0, 10));
-
-        arq.modify(1, 3, &10);
-        arq.modify(3, 5, &1);
-
-        assert_eq!(arq.query(0, 9), (23, 10));
-    }
 
     #[test]
     fn test_rmq() {
@@ -216,5 +275,41 @@ mod test {
         arq.modify(1, 6, &1);
 
         assert_eq!(arq.query(0, 9), -3);
+    }
+
+    #[test]
+    fn test_rmq_binary_search() {
+        let vec = vec![0, 1, -2, 3, -4, -5, 6, -7];
+        let mut arq = ArqTree::<AssignMin>::new(vec);
+        let pos = first_negative(&mut arq);
+
+        arq.modify(2, 7, &0);
+        let pos_zeros = first_negative(&mut arq);
+
+        assert_eq!(pos, 2);
+        assert_eq!(pos_zeros, -1);
+    }
+    
+    #[test]
+    fn test_range_sum() {
+        let mut arq = ArqTree::<AssignSum>::new(vec![(0, 1); 10]);
+
+        assert_eq!(arq.query(0, 9), (0, 10));
+
+        arq.modify(1, 3, &10);
+        arq.modify(3, 5, &1);
+
+        assert_eq!(arq.query(0, 9), (23, 10));
+    }
+    
+    #[test]
+    fn test_supply_demand() {
+        let mut arq = ArqTree::<SupplyDemand>::new(vec![(0, 0, 0); 10]);
+        
+        arq.modify(1, 1, &(25, 100));
+        arq.modify(3, 3, &(100, 30));
+        arq.modify(9, 9, &(0, 20));
+
+        assert_eq!(arq.query(0, 9), (125, 150, 75));
     }
 }
