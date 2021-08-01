bb154dc07a31a5120a8691bf5493b55c48d58fac
Eric Zhang <ekzhang1@gmail.com>
2021-08-01 10:39:59 -0500

    Fix name typo, add docs, and add reference test

--- src/rng.rs
+++ src/rng.rs
@@ -1,20 +1,24 @@
 //! Pseudorandom number generators (PRNGs).
 
 /// A simple and efficient random number generator.
-pub type SimpleRng = Xorshiro256PlusPlus;
+pub type SmallRng = Xoshiro256PlusPlus;
 
-/// A xorshiro256++ random number generator.
+/// A xoshiro256++ random number generator.
 ///
-/// This is a simplified version of the `SimpleRng` implementation from the
+/// This is a simplified version of the `SmallRng` implementation from the
 /// excellent `rand` crate, keeping only essential features.
 ///
-/// Source: https://docs.rs/rand/0.8.4/src/rand/rngs/xoshiro256plusplus.rs.html
+/// The xoshiro256++ algorithm is not suitable for cryptographic purposes, but
+/// is very fast and has excellent statistical properties.
+///
+/// * Source: [Docs.rs](https://docs.rs/rand/0.8.4/src/rand/rngs/xoshiro256plusplus.rs.html)
+/// * Theory: [Xorshift - Wikipedia](https://en.wikipedia.org/wiki/Xorshift)
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Xorshiro256PlusPlus {
+pub struct Xoshiro256PlusPlus {
     s: [u64; 4],
 }
 
-impl Xorshiro256PlusPlus {
+impl Xoshiro256PlusPlus {
     /// Construct a new RNG from a 64-bit seed.
     pub fn new(mut state: u64) -> Self {
         const PHI: u64 = 0x9e3779b97f4a7c15;
@@ -64,8 +68,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_xorshiro256plusplus() {
-        let mut rng = Xorshiro256PlusPlus::new(42);
+    fn test_xoshiro256plusplus() {
+        let mut rng = Xoshiro256PlusPlus::new(42);
         assert_eq!(rng.next_u64(), 15021278609987233951);
         assert_eq!(rng.next_u64(), 5881210131331364753);
         assert_eq!(rng.next_u64(), 18149643915985481100);
@@ -74,5 +78,27 @@ mod tests {
         assert_eq!(rng.next_u64(), 10848501901068131965);
         assert_eq!(rng.next_u64(), 2312344417745909078);
         assert_eq!(rng.next_u64(), 11162538943635311430);
+    }
+
+    #[test]
+    fn reference() {
+        let mut rng = Xoshiro256PlusPlus { s: [1, 2, 3, 4] };
+        // These values were produced with the reference implementation:
+        // http://xoshiro.di.unimi.it/xoshiro256plusplus.c
+        let expected = [
+            41943041,
+            58720359,
+            3588806011781223,
+            3591011842654386,
+            9228616714210784205,
+            9973669472204895162,
+            14011001112246962877,
+            12406186145184390807,
+            15849039046786891736,
+            10450023813501588000,
+        ];
+        for &e in &expected {
+            assert_eq!(rng.next_u64(), e);
+        }
     }
 }
